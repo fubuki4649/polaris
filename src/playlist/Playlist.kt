ca3b6f501c69417a92bc150693b565771ad19f47
fubuki4649 <boku@kaneki.dev>
2025-01-06 00:53:54 -0500

    Write metadata to songs

--- src/playlist/Playlist.kt
+++ src/playlist/Playlist.kt
@@ -6,7 +6,7 @@ import kotlin.io.path.*
 
 class Playlist(playlistLink: String, private val workPath: String = "~/.cache/polaris", overwrite: Boolean = false) {
 
-    private val ytDlpCommand = "yt-dlp -f bestaudio/best --extract-audio --audio-format aac --audio-quality 0 -o \"%(uploader)s <DELIMITER/> %(title)s\" --no-playlist --paths $workPath/audio '$playlistLink'"
+    private val ytDlpCommand = "yt-dlp -f bestaudio/best --extract-audio --audio-format aac --audio-quality 0 -o \"%(uploader)s <DELIMITER> %(title)s\" --no-playlist --paths $workPath/audio '$playlistLink'"
 
     val tracks: MutableList<Track> = mutableListOf()
 
--- src/playlist/Track.kt
+++ src/playlist/Track.kt
@@ -1,6 +1,10 @@
 package playlist
 
 import kotlinx.serialization.Serializable
+import org.jaudiotagger.audio.AudioFileIO
+import org.jaudiotagger.tag.FieldKey
+import org.jaudiotagger.tag.mp4.Mp4Tag
+import java.io.File
 
 class Track(val path: String) {
 
@@ -10,7 +14,7 @@ class Track(val path: String) {
     lateinit var metadata: Metadata
 
     init {
-        val splitPath = path.split("<DELIMITER/>")
+        val splitPath = path.split("<DELIMITER>")
         channelName = splitPath.first()
         videoName = splitPath.last()
     }
@@ -21,5 +25,22 @@ class Track(val path: String) {
         val artists: List<String> = emptyList(),
         val isCover: Boolean = false,
     )
+
+    fun writeMetadata() {
+
+        val audioFile = AudioFileIO.read(File(path))
+        val tag = audioFile.tagOrCreateDefault as Mp4Tag
+
+        // Set appropriate metadata fields
+        tag.setField(FieldKey.TITLE, metadata.title)
+        when(metadata.artists.size) {
+            0 -> {}
+            1 -> tag.setField(FieldKey.ARTIST, metadata.artists[0])
+            else -> tag.setField(FieldKey.ARTISTS, metadata.artists.joinToString("; "))
+        }
+
+        audioFile.commit()
+
+    }
 
 }
